dfc9c41f64ed67d04fe73344a4392622b8c92bc1
Jonathan Bins <56841987+jonathan-bins@users.noreply.github.com>
2021-09-10 11:31:25 -0700

    Add files via upload

---
+++ Exercise One/src/ExcerciseOne.java
@@ -0,0 +1,12 @@
+
+public class ExcerciseOne {
+	public static void main(String[] args) {
+		int oddNum = 1;
+		
+		do {
+			System.out.println(oddNum);
+			oddNum += 2;
+		}
+		while (oddNum <= 9);
+	}
+}
